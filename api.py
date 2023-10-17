035ef0bc11d4a3804344db8f2d11b11a13fb53ab
LWL220167529 <220167529@stu.vtc.edu.hk>
2023-10-17 17:50:34 +0800

    "api"

--- api.py
+++ api.py
@@ -1,40 +1,77 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session
 from flask_cors import CORS
 import json
-from customer import Customer
-from user import User
-from restaurant import Restaurant
-from deliveryPersonnel import DeliveryPersonnel
+    
+app = Flask(__name__, template_folder='templates')
+app.secret_key = 'id' # set your secret key here
+app.secret_key = 'name' # set your secret key here
 
-class main:
-    app = Flask(__name__, template_folder='templates')
-    app.secret_key = 'user' # set your secret key here
-    user = None
-    CORS(app)
-
-    @app.route('/')
-    def home(user=None):
-        user = session.get('user')
-        with open('food.json', 'r') as f:
+CORS(app)
+@app.route('/login', methods=['GET', 'POST'])
+def customerLogin():
+    if request.method == 'POST':
+        with open('user.json', 'r') as f:
             data = json.load(f)
-        if user is None:
-            return render_template('YummyRestaurant.html', data=data)
-        else:
-            return render_template('YummyRestaurant.html', data=data)
+        for customer in data["customer"]:
+            if customer['id'] == request.form['email'] or customer['email'] == request.form['email'] and customer['password'] == request.form['password']:
+                session['id'] = customer['id']
+                session['name'] = customer['name']
+                session['tab'] = 'customer'
+                return redirect(url_for('home'))
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
 
+@app.route('/login/restaurant', methods=['GET', 'POST'])
+def restaurantLogin():
+    if request.method == 'POST':
+        with open('user.json', 'r') as f:
+            data = json.load(f)
+        for customer in data["restaurant"]:
+            if customer['id'] == request.form['email'] or customer['email'] == request.form['email'] and customer['password'] == request.form['password']:
+                session['id'] = customer['id']
+                session['name'] = customer['name']
+                session['tab'] = 'restaurant'
+                return redirect(url_for('home'))
+        return render_template('loginRestaurant.html')
+    else:
+        return render_template('loginRestaurant.html')
 
-    @app.route('/login', methods=['GET', 'POST'])
-    def customerLogin():
-        if request.method == 'POST':
-            with open('user.json', 'r') as f:
+@app.route('/login/deliveryPersonnel', methods=['GET', 'POST'])
+def deliveryPersonnelLogin():
+    if request.method == 'POST':
+        with open('user.json', 'r') as f:
+            data = json.load(f)
+        for customer in data["restaurant"]:
+            if customer['id'] == request.form['email'] or customer['email'] == request.form['email'] and customer['password'] == request.form['password']:
+                session['id'] = customer['id']
+                session['name'] = customer['name']
+                session['tab'] = 'deliveryPersonnel'
+                return redirect(url_for('home'))
+        return render_template('loginDeliveryPersonnel.html')
+    else:
+        return render_template('loginDeliveryPersonnel.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('id', None)
+    session.pop('name', None)
+    session.pop('tab', None)
+    return redirect(url_for('home'))
+
+@app.route('/')
+def home(user=None):
+        if 'id' and 'name' and 'tab' not in session:
+            with open('food.json', 'r') as f:
                 data = json.load(f)
-            for customer in data["customer"]:
-                if customer['id'] == request.form['email'] or customer['email'] == request.form['email'] and customer['password'] == request.form['password']:
-                    user = Customer(customer['name'], customer['email'], customer['id'], customer['password'], customer['address'])
-                    session['user'] = user
-                return redirect(url_for('home'), user=user)
+            return render_template('YummyRestaurant.html', data=data)
         else:
-            return render_template('login.html')
+            userID = session['id']  # get user dictionary from session
+            username = session['name']  # get user dictionary from session
+            tab = session['tab']  # get user dictionary from session
+            with open('food.json', 'r') as f:
+                data = json.load(f)
+            return render_template('YummyRestaurant.html', data=data, name=username, id=userID, tab=tab)
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- deliveryPersonnel.py
+++
@@ -1,6 +0,0 @@
-from user import User
-import json
-
-class DeliveryPersonnel(User):
-    def __init__(self, name, email, id, password):
-        super().__init__(name, email, id, password)
