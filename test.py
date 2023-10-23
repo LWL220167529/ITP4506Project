74abac36b7b2fa1cfccba3d9b87f3b3a84bd7dd5
LWL220167529 <220167529@stu.vtc.edu.hk>
2023-10-23 14:51:56 +0800

    hi

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-import json
-
-for i in range(5):
-    print(str(i))
+        userID = session['id']
+        with open('order.json', 'r') as f:
+            order = json.load(f)
+        # add cart to existing order
+        for userID in order:
+            if userID == session['id']:
+                for orderID in order[userID]:
+                    if orderID['orderId'] == 1:
+                        orderID['order'].append(cart)
+                        break
+                else:
+                    order[userID].append({"orderId": order[userID][-1]['orderId'] + 1, "order": [cart], "status": "pending"})
+        else:
+            order.append({session['id']: {"orderId": 1, "order": {cart}}})
